6295adb98fc41239c7d2fa0911980902cb57484a
Yair <jacklove6p6@gmail.com>
2026-06-17 16:04:05 +0300

    Working on the details page still

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -19,22 +19,57 @@ return (
   <section className="stay-details">
     <Link to="/homes">Back to list</Link>
     <h1>Stay Details</h1>
-    <div>
-      <h3>{stay.name}</h3>
-      <div className = "details-gallery">
-     {stay.imgUrls.map((url, index) => (
-    <img 
-        key={index}
-        src={url} 
-        alt={stay.name} 
-        className="stay-card-img" 
-    />
-))}
-</div>
-      {stay.host && <h3>Host: {stay.host.fullname}</h3>}
-      <h4>${stay.price} / night</h4>
-      {/* <pre>{JSON.stringify(stay, null, 2)}</pre> */}
+    
+    <div className="details-gallery">
+      {stay.imgUrls.map((url, index) => (
+        <img 
+          key={index}
+          src={url} 
+          alt={stay.name} 
+          className="stay-card-img" 
+        />
+      ))}
     </div>
+
+    <main className="stay-content-container">
+      
+      <section className="stay-info-main">
+        
+        <header className="stay-overview-header">
+          <h4>{stay.name}</h4>
+          <ul className = "stay-info-list"><li> {stay.bedrooms}</li><li>{stay.bathrooms}</li></ul>
+          <h4>${stay.price} / night</h4>
+        </header>
+
+        <section className="further-details">
+          
+          {/* <section className="host-profile">
+            <h3>Hosted by Kaylee</h3> 
+            {stay.host && <h3>Host: {stay.host.fullname}</h3>}
+            <p>Superhost · 3 years hosting</p>
+          </section> */}
+
+          <section className="stay-highlights">
+            <h3>Home highlights</h3> 
+            <ul>
+              <li>Self check-in</li>
+              <li>Amazing outdoor space</li>
+            </ul>
+          </section>
+
+          <section className="stay-description">
+            <h3>Description</h3> 
+            <p>Welcome to a luxurious family retreat...</p>
+          </section>
+
+        </section> 
+      </section> 
+
+      <aside className="booking-sidebar">
+      </aside>
+
+    </main>
+
   </section>
 )
 }
