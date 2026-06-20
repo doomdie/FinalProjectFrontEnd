cee11d589e96ee23610f347b7f43822d90b044f4
Yair <jacklove6p6@gmail.com>
2026-06-20 12:51:36 +0300

    Annoying as hell css fixes

--- src/cmps/GuestMenu.jsx
+++ src/cmps/GuestMenu.jsx
@@ -36,12 +36,14 @@ export function GuestMenu({ currentList, onUpdateList, stay }) {
 
   return (
     <div className="guest-triggers">
-      <Button
-        variant="contained"
+      <Box
+        className="guest-trigger-tile"
         onClick={handleButtonClick}
+        role="button"
+
       >
-        GUEST AMOUNT
-      </Button>
+        <span className="tile-title">GUESTS</span>
+      </Box>
 
       <Menu
         anchorEl={anchorEl}
@@ -62,12 +64,12 @@ export function GuestMenu({ currentList, onUpdateList, stay }) {
             gap: '16px',
             width: '100%'
           }}>
-          
+
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-    <span>ADULTS</span>
-    <span style={{ fontSize: '0.75rem', color: '#666' }}>Ages 13 or above</span> 
-  </Box>
-  {/* The styles within the code itself are probably frowned upon but I can't seem to get it to work in normal css.. Fuuuckk */}
+            <span>ADULTS</span>
+            <span style={{ fontSize: '0.75rem', color: '#666' }}>Ages 13 or above</span>
+          </Box>
+          {/* The styles within the code itself are probably frowned upon but I can't seem to get it to work in normal css.. Fuuuckk */}
           <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
             <IconButton
               size="small"
