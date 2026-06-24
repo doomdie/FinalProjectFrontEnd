273e815ef86c333c138cbe645ebd54f906d54e48
Yair <jacklove6p6@gmail.com>
2026-06-24 13:23:09 +0300

    more stuff

--- src/pages/HostMenu.jsx
+++ src/pages/HostMenu.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom'
+
 import myLocalImage from '../data/image.avif'; // Where the heck do i put the image folder professionally again like.. Remember to ask boris!! Or the teacher
 
 export function HostMenu() {
@@ -8,7 +10,10 @@ export function HostMenu() {
             <img src={myLocalImage} alt="Host reservation" />
             </div>
             <h1>You don't have any reservations</h1>
-            
+            <h2>To get booked, you'll need to complete and publish your listing.</h2>
+            <NavLink to="/become-a-host">
+						hostmode
+					</NavLink>
         </div>
     );
 }
