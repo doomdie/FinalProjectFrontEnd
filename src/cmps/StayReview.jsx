67632deb2067c3d56fec05277805b4db305318b0
Yair <jacklove6p6@gmail.com>
2026-06-27 13:03:53 +0300

    primitive review i realized im on main

--- src/cmps/StayReview.jsx
+++ src/cmps/StayReview.jsx
@@ -2,9 +2,10 @@ export function StayReview({ stay }) {
     if (!stay || !stay.reviews) return <div>Loading reviews...</div>
     console.log(stay)
     return (
-        <div>
-            {stay.reviews[1]?.txt}
-            <div>YOOOO</div>
-        </div>
+        // <div>
+        //     {stay.reviews[1]?.txt}
+        //     <div>YOOOO</div>
+        // </div>
+        <div></div>
     )
 }
